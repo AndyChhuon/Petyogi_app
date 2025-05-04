9d37b16bc33c5eb3434ad8833de4036be16f6c70
Andy Chhuon <chhuonandy@gmail.com>
2025-05-04 10:34:55 -0400

    Remove dev config

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -8,14 +8,16 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// input your firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBzZBdtRHmaIP6ZL4cFJ338filXok2DmQE",
-  authDomain: "petyogi-9ebc5.firebaseapp.com",
-  projectId: "petyogi-9ebc5",
-  storageBucket: "petyogi-9ebc5.appspot.com",
-  messagingSenderId: "813978453201",
-  appId: "1:813978453201:web:bd30a5bf6b9fd9dd18b1f0",
-  measurementId: "G-Q22H1Q7XGV",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
